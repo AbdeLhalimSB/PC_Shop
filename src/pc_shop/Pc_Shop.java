c1347c3d603ad3c9355f795bcc2f58577b07bc5f
AbdeLhalimSB <AbdeLhalimSB>
2022-06-19 20:29:45 +0100

    Second Commit

---
+++ src/pc_shop/Pc_Shop.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package pc_shop;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author sahba
+ */
+public class Pc_Shop extends Application {
+    
+    @Override
+    public void start(Stage stage) throws IOException {
+        //try {
+            Parent root = FXMLLoader.load(getClass().getResource("/pc_shop/View/Services.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        //} catch (IOException ex) {
+            //Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        //}
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+  
+    
+}
