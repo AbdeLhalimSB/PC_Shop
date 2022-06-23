f9b91ffa8fe05f0aafc3c21be90b167cfebd6aef
AbdeLhalimSB <AbdeLhalimSB>
2022-06-23 03:07:27 +0100

    Add Products page

---
+++ src/pc_shop/Control/ViewControle/HomeControle.java
@@ -0,0 +1,70 @@
+
+package pc_shop.Control.ViewControle;
+
+import java.io.IOException;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author sahba
+ */
+public class HomeControle {
+    
+    @FXML
+    Button btn_prod;
+    @FXML
+    Button btn_serv;
+    @FXML
+    Button btn_stat;
+    @FXML
+    Button btn_emp;
+    @FXML
+    Button btn_home;
+    @FXML
+    Button btn_exit;
+    
+    public void openProducts(Event e) throws IOException{
+        Node node = (Node) e.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();                  
+            stage.close();
+            Parent root = FXMLLoader.load(getClass().getResource("/pc_shop/View/Products.fxml"));       
+            Scene scene = new Scene(root);       
+            stage.setScene(scene);
+            stage.show();
+    }
+    public void openServices(Event e) throws IOException{
+        Node node = (Node) e.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();                  
+            stage.close();
+            Parent root = FXMLLoader.load(getClass().getResource("/pc_shop/View/Products.fxml"));       
+            Scene scene = new Scene(root);       
+            stage.setScene(scene);
+            stage.show();
+    }
+    public void openStatistice(Event e) throws IOException{
+        Node node = (Node) e.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();                  
+            stage.close();
+            Parent root = FXMLLoader.load(getClass().getResource("/pc_shop/View/Products.fxml"));       
+            Scene scene = new Scene(root);       
+            stage.setScene(scene);
+            stage.show();
+    }
+    public void openEmployee(Event e) throws IOException{
+        Node node = (Node) e.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();                  
+            stage.close();
+            Parent root = FXMLLoader.load(getClass().getResource("/pc_shop/View/Products.fxml"));       
+            Scene scene = new Scene(root);       
+            stage.setScene(scene);
+            stage.show();
+    }
+}
